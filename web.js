960cabe40646ab792f5a2bc989ffd9a2b6ee9e23
treyford42 <treyford@gmail.com>
2013-07-09 16:59:04 -0500

    Fixed issue with web.js

--- web.js
+++ web.js
@@ -2,6 +2,8 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
 app.get('/', function(request, response) {
   //response.send('Hello World 2');
 	response.send(fs.readFileSync("index.html").toString(););
