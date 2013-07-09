71391f17fd5c489c933654b4480575927f464295
treyford42 <treyford@gmail.com>
2013-07-09 17:04:09 -0500

    Fixed issue with web.js

--- web.js
+++ web.js
@@ -2,11 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var fs = require('fs');
+var fs = require('fs'); 
 
 app.get('/', function(request, response) {
   //response.send('Hello World 2');
-	response.send(fs.readFileSync("index.html").toString(););
+	response.send(fs.readFileSync("index.html").toString());
 });
 
 var port = process.env.PORT || 5000;
